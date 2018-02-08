99342fc56516a0564f907c097d857474d9b5427d
cnwutianhao <cnwutianhao@163.com>
2018-02-08 17:21:34 +0800

    Add StreetView

---
+++ app-streetview/src/main/java/com/haocent/android/googlemap/StreetViewActivity.java
@@ -0,0 +1,33 @@
+package com.haocent.android.googlemap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class StreetViewActivity extends AppCompatActivity {
+
+    private static final LatLng DALIAN = new LatLng(38.9135883, 121.6148269);
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_street_view);
+
+        SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
+                (SupportStreetViewPanoramaFragment)
+                        getSupportFragmentManager().findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(
+                new OnStreetViewPanoramaReadyCallback() {
+                    @Override
+                    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+                        if (savedInstanceState == null) {
+                            panorama.setPosition(DALIAN);
+                        }
+                    }
+                });
+    }
+}
