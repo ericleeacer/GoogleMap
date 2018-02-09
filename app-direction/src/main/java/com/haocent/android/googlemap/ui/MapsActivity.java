eaa63c4ac4d8e0fe0e083d970d24a92504fd508e
cnwutianhao <cnwutianhao@163.com>
2018-02-09 10:46:28 +0800

    Google Map Direction

--- app-direction/src/main/java/com/haocent/android/googlemap/ui/MapsActivity.java
+++ app-direction/src/main/java/com/haocent/android/googlemap/ui/MapsActivity.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
