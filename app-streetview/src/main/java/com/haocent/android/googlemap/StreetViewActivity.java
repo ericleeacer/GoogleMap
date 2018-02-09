076db0af903a7880a1a332ced3897e101703152c
cnwutianhao <cnwutianhao@163.com>
2018-02-09 10:08:00 +0800

    StreetView

--- app-streetview/src/main/java/com/haocent/android/googlemap/StreetViewActivity.java
+++ app-streetview/src/main/java/com/haocent/android/googlemap/StreetViewActivity.java
@@ -9,19 +9,19 @@ import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
- * 街景（基础）
+ * 街景
  *
  * Created by Tnno Wu on 2018/02/08.
  */
 
-public class StreetViewBasicActivity extends AppCompatActivity {
+public class StreetViewActivity extends AppCompatActivity {
 
-    private static final LatLng DALIAN = new LatLng(38.9135883, 121.6148269);
+    private static final LatLng STATUE_OF_LIBERTY = new LatLng(40.689611718944896, -74.04564142227173);
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_street_view_basic);
+        setContentView(R.layout.activity_street_view);
 
         SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
                 (SupportStreetViewPanoramaFragment)
@@ -31,7 +31,7 @@ public class StreetViewBasicActivity extends AppCompatActivity {
                     @Override
                     public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
                         if (savedInstanceState == null) {
-                            panorama.setPosition(DALIAN);
+                            panorama.setPosition(STATUE_OF_LIBERTY);
                         }
                     }
                 });
