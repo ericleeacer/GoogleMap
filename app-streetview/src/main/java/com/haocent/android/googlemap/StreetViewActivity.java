8d968d60ea6d3ad2815b1c3fa81daf4e91af8dad
cnwutianhao <cnwutianhao@163.com>
2018-02-09 10:25:44 +0800

    Google Map StreetView

--- app-streetview/src/main/java/com/haocent/android/googlemap/StreetViewActivity.java
+++ app-streetview/src/main/java/com/haocent/android/googlemap/StreetViewActivity.java
@@ -9,7 +9,7 @@ import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
- * 街景
+ * 街景（基础）
  *
  * Created by Tnno Wu on 2018/02/08.
  */
