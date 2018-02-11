1625f1d6747aefb3a241f0ec1c5113626e970a38
cnwutianhao <cnwutianhao@163.com>
2018-02-11 09:09:40 +0800

    Google Map Direction

--- app-direction/src/main/java/com/haocent/android/googlemap/data/Constant.java
+++ app-direction/src/main/java/com/haocent/android/googlemap/data/Constant.java
@@ -8,7 +8,10 @@ public class Constant {
 
     public static final String GOOGLE_API_KEY = "AIzaSyCsSXwWlWn0JrjVtKAv1DWmrkTkI3sgrYs";
 
-    public static final String TRAVEL_MODE = "drive";
+    public static final String TRAVEL_MODE = "driving";     // 驾车
+//    public static final String TRAVEL_MODE = "walking";     // 步行
+//    public static final String TRAVEL_MODE = "bicycling";   // 骑行
+//    public static final String TRAVEL_MODE = "transit";     // 公交
 
     public static final String LANGUAGE = "zh-CN";
 }
