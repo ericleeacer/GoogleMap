24ab94303064277ee5bacc79d485496b552df208
cnwutianhao <cnwutianhao@163.com>
2018-02-13 11:29:57 +0800

    逻辑改为：地点自动填充

--- app-latlng/src/main/java/com/haocent/android/latlng/ui/MainActivity.java
+++ app-latlng/src/main/java/com/haocent/android/latlng/ui/MainActivity.java
@@ -1,15 +1,22 @@
 package com.haocent.android.latlng.ui;
 
-import android.app.ProgressDialog;
-import android.support.design.widget.TextInputEditText;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.haocent.android.latlng.R;
 import com.haocent.android.latlng.api.GoogleMapService;
 import com.haocent.android.latlng.data.Constant;
@@ -28,12 +35,15 @@ public class MainActivity extends AppCompatActivity {
 
     private GoogleMapService mService;
 
-    ProgressDialog mProgressDialog;
-
-    private TextInputEditText mEditText;
-    private Button mBtn;
-    private TextView mTvAddress, mTvLat, mTvLng;
+    private CardView mCardView;
+    private TextView mTvAddress, mTvLat, mTvLng, mTvPlaceName;
+    private Button mBtnSearch;
     private LinearLayout mLinearLayout;
+    private ProgressBar mProgressBar;
+
+    private String placeName;
+
+    private int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         initService();
 
-        initProgress();
-
         initView();
     }
 
@@ -51,39 +59,45 @@ public class MainActivity extends AppCompatActivity {
         mService = new RetrofitClient().getService();
     }
 
-    private void initProgress() {
-        mProgressDialog = new ProgressDialog(this);
-    }
-
     private void initView() {
-        mEditText = findViewById(R.id.edit_input);
-        mBtn = findViewById(R.id.btn);
+        mCardView = findViewById(R.id.card_view);
+        mBtnSearch = findViewById(R.id.btn);
         mTvAddress = findViewById(R.id.tv_formatted_address);
         mTvLat = findViewById(R.id.tv_lat);
         mTvLng = findViewById(R.id.tv_long);
+        mTvPlaceName = findViewById(R.id.tv_place_name);
         mLinearLayout = findViewById(R.id.ll);
+        mProgressBar = findViewById(R.id.progress);
 
+        mBtnSearch.setVisibility(View.GONE);
         mLinearLayout.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.GONE);
 
-        mBtn.setOnClickListener(new View.OnClickListener() {
+        mCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showProgress();
+                try {
+                    Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(MainActivity.this);
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        mBtnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProgressBar.setVisibility(View.VISIBLE);
 
                 initData();
             }
         });
     }
 
-    private void showProgress() {
-        mProgressDialog.setMessage("正在查询");
-        mProgressDialog.setCancelable(false);
-        mProgressDialog.show();
-    }
-
     private void initData() {
         Observable<GoogleMapDataBean> observable = mService.getData(
-                mEditText.getText().toString(),
+                placeName,
                 Constant.GOOGLE_API_KEY,
                 Constant.LANGUAGE
         );
@@ -113,14 +127,33 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete() {
                         Log.d(TAG, "onComplete: ");
 
-                        hideProgress();
+                        mProgressBar.setVisibility(View.GONE);
 
                         mLinearLayout.setVisibility(View.VISIBLE);
                     }
                 });
     }
 
-    private void hideProgress() {
-        mProgressDialog.hide();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                placeName = place.getName().toString();
+
+                if (TextUtils.isEmpty(placeName)) {
+                    mTvPlaceName.setText("输入查询的地点");
+                    mBtnSearch.setVisibility(View.GONE);
+                } else {
+                    mTvPlaceName.setText(placeName);
+                    mBtnSearch.setVisibility(View.VISIBLE);
+                }
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.d(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+
+            }
+        }
     }
 }
